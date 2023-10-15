27e28a4a464cb906f89bb56d56bb7b7ea137e210
Yugank Bansal <yugankbansal.00001276033.aesl@gmail.com>
2023-10-15 17:31:25 +0530

    refactor: inventory page added

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 import CardsInventory from "@/components/cardsInventory";
 import Navbar from "@/components/hostee/Navbar";
+import Rewards from "@/components/inventory/Rewards"
+import Events from "@/components/inventory/Events"
+import MyCalender from "@/components/inventory/MyCalender"
 import { fetchUsername } from "@/utils";
 import { useEffect, useState } from "react";
 import { useWallet } from "@solana/wallet-adapter-react";
@@ -32,9 +35,31 @@ const EventsByHost = () => {
     }
 
     return (
-        <div className="bg-[#25143a] h-[100%] text-white w-full overflow-hidden">
+        <div className=" text-white w-full overflow-hidden">
             <Navbar />
-            <div>
+            <div className="flex justify-between px-[6rem]">
+                <div className="pb-8 flex flex-col gap-1">
+                    <div>
+                        <p className="text-2xl font-semibold">Inventory</p>
+                    </div>
+                    <p className="opacity-75">Explore the events you&apos;e marked as yours.</p>
+                </div>
+                <input
+                    type="text"
+                    placeholder="Search events"
+                    className="text-white h-[40px] border-white w-[20rem] bg-[#1C1C1C]  rounded-full px-4 "
+                />
+            </div>
+            <Rewards />
+
+            <div className="w-full mt-[5rem]">
+                <div className="flex">
+                    <Events />
+                    <MyCalender/>
+                </div>
+            </div>
+
+            {/* <div>
                 <div className="flex justify-between px-[6rem]">
                     <div className="pb-8 flex flex-col gap-1">
                         <div>
@@ -108,7 +133,7 @@ const EventsByHost = () => {
                         username={username}
                     />
                 </div>
-            </div>
+            </div> */}
         </div>
     );
 };
